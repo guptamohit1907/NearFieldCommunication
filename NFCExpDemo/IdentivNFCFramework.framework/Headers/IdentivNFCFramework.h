0ba9ff28468bcfe41f121783704e81efdab9740a
Mohit Gupta <Mohit.Gupta14@ibm.com>
2023-03-01 03:07:00 +0530

    Initial code Setup

---
+++ NFCExpDemo/IdentivNFCFramework.framework/Headers/IdentivNFCFramework.h
@@ -0,0 +1,18 @@
+//
+//  IdentivNFCFramework.h
+//  IdentivNFCFramework
+//
+//  Created by Van Do on 5/21/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IdentivNFCFramework.
+FOUNDATION_EXPORT double IdentivNFCFrameworkVersionNumber;
+
+//! Project version string for IdentivNFCFramework.
+FOUNDATION_EXPORT const unsigned char IdentivNFCFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IdentivNFCFramework/PublicHeader.h>
+
+
